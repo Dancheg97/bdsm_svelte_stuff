5c566d8d964f8ae44358c1bf0d450ef8fac18d48
Danila Fomin <dangdancheg@gmail.com>
2022-07-09 18:49:03 +0300

    separated desktop app from mobile

---
+++ src/routes/Desktop/Desktop.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Content from '../Content/Content.svelte';
+	import SideBar from '../SideBar/SideBar.svelte';
+</script>
+
+<div class="flex-container">
+	<SideBar />
+	<Content />
+</div>
+
+<style>
+	.flex-container {
+		display: flex;
+		flex-direction: row;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,8 @@
-<script>
-	import SideBar from './SideBar/SideBar.svelte';
-	import Content from './Content/Content.svelte';
-</script>
-
-<div class="flex-container">
-	<SideBar />
-	<Content />
-</div>
 
 <style>
 	:global(body) {
 		background-color: #1d3040;
 		color: #bfc2c7;
 	}
-	.flex-container {
-		display: flex;
-		flex-direction: row;
-	}
+	
 </style>
