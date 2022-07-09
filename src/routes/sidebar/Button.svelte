cdd72ad84fcbe74488f51b54d4daf6655c32b22d
Danila Fomin <dangdancheg@gmail.com>
2022-07-09 15:39:55 +0300

    added properly working sidebar

--- src/routes/sidebar/Button.svelte
+++ src/routes/sidebar/Button.svelte
@@ -1,18 +1,18 @@
 <script>
 	export let src = 'https://icon-library.com/images/logout-icon-png/logout-icon-png-13.jpg';
-	function onClick() {
+	export function onClick() {
 		alert('clicked');
 	}
 </script>
 
 <!-- svelte-ignore a11y-img-redundant-alt -->
-<img {src} alt="image is loading" />
+<img {src} alt="image is loading" on:click={onClick} />
 
 <style>
 	img {
 		height: 45px;
 		width: 45px;
-		border-radius: 8px;
+		border-radius: 17px;
 		padding: 4px;
 	}
 </style>
--- src/routes/sidebar/SideBar.svelte
+++ src/routes/sidebar/SideBar.svelte
@@ -1,18 +1,36 @@
 <script>
 	import Button from './Button.svelte';
 
-	const srcs = [
-		'https://thumbs.dreamstime.com/b/man-hipster-avatar-cartoon-guy-black-hair-man-hipster-avatar-cartoon-guy-black-hair-flat-icon-blue-background-user-223717055.jpg',
-		'https://icon-library.com/images/white-home-icon-png/white-home-icon-png-21.jpg',
-		'https://icon-library.com/images/white-settings-icon/white-settings-icon-1.jpg',
-		'https://icon-library.com/images/logout-icon-png/logout-icon-png-13.jpg'
-	];
+	let userImg =
+		'https://thumbs.dreamstime.com/b/man-hipster-avatar-cartoon-guy-black-hair-man-hipster-avatar-cartoon-guy-black-hair-flat-icon-blue-background-user-223717055.jpg';
+	let homeImage = 'https://icon-library.com/images/white-home-icon-png/white-home-icon-png-21.jpg';
+
+	let settingsImage =
+		'https://icon-library.com/images/white-settings-icon/white-settings-icon-1.jpg';
+	let exitImage = 'https://icon-library.com/images/logout-icon-png/logout-icon-png-13.jpg';
 </script>
 
 <ul>
-	{#each srcs as src}
-		<li>
-			<Button src />
-		</li>
-	{/each}
+	<li>
+		<Button src={userImg} />
+	</li>
+	<hr />
+	<li>
+		<Button src={homeImage} />
+	</li>
+	<li>
+		<Button src={settingsImage} />
+	</li>
+	<hr />
+	<li>
+		<Button src={exitImage} />
+	</li>
 </ul>
+
+<style>
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+</style>
