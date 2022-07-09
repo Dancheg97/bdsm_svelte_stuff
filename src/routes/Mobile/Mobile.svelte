813ad4d579936957e58c77c5e410d2a7f68ae518
DANCHE <dangdancheg@gmail.com>
2022-07-09 22:56:43 +0300

    added accordion transition for top button for mobile version

--- src/routes/Mobile/Mobile.svelte
+++ src/routes/Mobile/Mobile.svelte
@@ -1,5 +1,30 @@
 <script>
 	import Content from './Content/Content.svelte';
+	import accordion from './TopBar/accordion.js';
 </script>
 
-<Content />
+<details use:accordion>
+	<summary>
+		<span>I'm an accordion, click me!</span>
+	</summary>
+	<div class="flex-container">
+		<Content />
+	</div>
+</details>
+
+<style>
+	.flex-container {
+		display: flex;
+		flex-direction: column;
+	}
+	details {
+		background: #001c41;
+		height: var(--collapsed);
+		overflow: hidden;
+		transition: height 300ms cubic-bezier(0.4, 0.01, 0.165, 0.99);
+		border-radius: 5px;
+	}
+	details[open] {
+		height: var(--expanded);
+	}
+</style>
---
+++ src/routes/Mobile/TopBar/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let src = 'https://icon-library.com/images/logout-icon-png/logout-icon-png-13.jpg';
+	export function onClick() {
+		alert('clicked');
+	}
+</script>
+
+<div>
+	<img {src} alt="" on:click={onClick} />
+</div>
+
+<style>
+	img {
+		height: 45px;
+		width: 45px;
+		border-radius: 17px;
+		padding: 4px;
+	}
+	div {
+		padding: 5px;
+	}
+</style>
